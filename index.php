2bbb58806c4f675fe245003b00f63b7e3c7052bb
Dio <primo.nicolas@outlook.be>
2015-05-28 15:29:49 +0200

    FTQP

---
+++ index.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <link type="text/css" rel="stylesheet" href="css/style.css" />
+        <link href='http://fonts.googleapis.com/css?family=Oswald' rel='stylesheet' type='text/css'>
+        <link href='http://fonts.googleapis.com/css?family=Oswald:300' rel='stylesheet' type='text/css'>
+        <title>
+            FTQP
+        </title>
+    </head>
+    <body>
+        <!--  -----------------------------------  -->
+        <div id="header">
+            <div id="placeur-contenu-header">
+                 <div id="logo"></div>
+                 <div id="texte">Formation de Travail en Quartier Populaire</div>
+            </div>     
+        </div>
+        <!--  -----------------------------------  -->
+
+        <div id="menu">
+            <div id="centrage-menu">
+                <ul>
+                    <li><a href="">ACCUEIL</a></li>
+                    <li><a href="">PAGE 2</a></li>
+                    <li><a href="">PAGE 3</a></li>
+                    <li><a href="">PAGE 4</a></li>
+                    <li><a href="">PAGE 5</a></li>
+                    
+                </ul>
+            </div>
+        </div>  
+        <!--  -----------------------------------  -->
+        <div id="conteneur">
+
+            <div id="contenu">
+                <titre>Présentation du centre FTQP asbl.</titre>
+                <texte>
+                    Le centre de formation FTQP propose depuis le 26/03/1979 à des demandeurs d'emploi peu qualifiés de plus de 18 ans (détendeurs du CEB ou du CESI) deux formations qualifiantes dans le secteur de la construction : l'une en maçonnerie et l'autre en plafonnage.<br/><br/>
+
+Le stagiaire en formation devra être capable d'atteindre deux niveaux de compétences distincts :<br/>
+     - Le niveau de base.<br/>
+     - Le niveau de perfectionnement.<br/>
+Le stagiaire évolue à son rythme et dispose d'un délai maximum de 2 ans (2080 heures max) au centre à raison de 1040 heures de formation et 152 heures de stage en entreprise par an.<br/>
+Une mise à l'emploi au terme de la formation finalise le processus d'insertion.<br/>
+
+
+<h1>FTQP ALTERNANCE : EMPLOI FORMATION.</h1>
+ 
+Pour la 2ème année de formation, possibilité pour le(la) stagiaire âgé de 18 à 25 ans d'entrer dans un système en alternance organisé sur base de 1 jour  de formation et guidance et 4 jours en entreprise sous contrat RAC (Régime Apprentissage Construction du Fonds de Formation de la Construction FFC/FVB.
+<div id="image1"></div>
+Avec le soutien de la Cocof, de Bruxelles-Formation, d'Actiris, du Fonds Social Européen et du Fonds de Formation de la Construction :
+<div id="partenaire-img">
+    <img src="images/partenaire/01.png"/>
+    <img src="images/partenaire/02.jpg"/>
+    <img src="images/partenaire/03.png"/>
+    <img src="images/partenaire/04.png"/>
+    <img src="images/partenaire/05.png"/>
+    <img src="images/partenaire/06.png"/>
+</div>
+<p>Objectif de la  formation qualifiante du maçon et du plafonneur </p><br/>
+
+<p>Le stagiaire devra être capable d'atteindre deux niveaux de compétences distincts : 1) le niveau de base; 2) le niveau de perfectionnement.  Le stagiaire évolue à son rythme et dispose d'un délai maximum de 2 ans (2080 heures) au centre à raison de 1040 heures par an et 304 heures de stage en entreprise à raison de 152 heures par an.</p>
+<p>La formation se déroule du lundi au vendredi de 8h30 à 16h00.</p><br/>
+ 
+<p>Le stagiaire devra être capable d'intégrer les compétences associées aux activités clés du maçon et du plafonneur au travers de la réalisation de modules successifs en atelier (apprentissage pratique) et aux cours généraux (apprentissage théorique) sous la tutelle des formateurs.</p><br/>
+ 
+<p>Les stages en entreprise.<br/>
+Nous intégrons à nos formations des stages en entreprise (non rémunérés pour le 1er stage d'immersion) de 152 heures (4 semaines) par niveau de formation.<br/>
+     - Le premier stage  consiste essentiellement en une première confrontation avec le milieu professionnel, il est centré sur le respect des règles du monde de l'entreprise et sur la mise en pratique sur chantier des apprentissages réalisés dans nos ateliers. Stage d'immersion.<br/>
+     - Le deuxième stage a pour objectif d'être un tremplin vers l'emploi, une sorte de période d'essai avant la signature d'un contrat à durée indéterminée.  Stage d'achèvement.</p>
+                </texte>
+            </div>
+
+        </div>
+        <!--  -----------------------------------  -->
+        <div id="footer"> 
+
+        </div>
+
+    </body>
+</html>
